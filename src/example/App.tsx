91a9f2099e379c4bb7e91da6103d67256d7f9223
Martin Kapal <flamecze@gmail.com>
2023-04-10 23:19:42 +0200

    Clean up ReactInSim renderer code

--- src/example/App.tsx
+++ src/example/App.tsx
@@ -5,25 +5,8 @@ import { Layouts, TypeIn } from './components';
 export function App() {
   useEffect(() => {
     console.log('App mounted');
-
     console.log('');
     // InSimRenderer.dumpTree();
-
-    // const a = setTimeout(() => {
-    //   setVariant('light');
-    //
-    //   // setButtonCount((prevState) => prevState + 1);
-    //   setIsShown(false);
-    // }, 1000);
-
-    // const timeout = setTimeout(() => {
-    // setIsShown(true);
-    // }, 1000);
-
-    return () => {
-      // clearTimeout(a);
-      // clearTimeout(timeout);
-    };
   }, []);
 
   return (
