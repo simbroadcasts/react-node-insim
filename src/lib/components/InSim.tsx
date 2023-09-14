a5712f79a03fa184ec9be59609de016498bb3ed4
Martin Kapal <flamecze@gmail.com>
2023-09-15 00:14:19 +0200

    Memoize event listener callbacks in InSim component

--- src/lib/components/InSim.tsx
+++ src/lib/components/InSim.tsx
@@ -5,8 +5,8 @@ import type {
 } from 'node-insim';
 import type { IS_CPP, IS_MSO, IS_STA, IS_VER } from 'node-insim/packets';
 import { PacketType } from 'node-insim/packets';
-import type { ReactNode } from 'react';
-import { useEffect } from 'react';
+import type { DependencyList, ReactNode } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 import { useInSim } from '../hooks';
 
@@ -41,11 +41,27 @@ function usePacketEventListener<T extends keyof InSimPacketEvents>(
   event: T,
   listener?: InSimPacketEvents[T],
 ) {
+  const listenerRef = useCallbackRef(listener);
+
   useEffect(() => {
-    listener && inSim.on(event, listener as InSimEvents[T]);
+    listener && inSim.on(event, listenerRef as InSimEvents[T]);
 
     return () => {
-      listener && inSim.off(event, listener as InSimEvents[T]);
+      listener && inSim.off(event, listenerRef as InSimEvents[T]);
     };
-  }, [listener]);
+  }, [listenerRef]);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function useCallbackRef<T extends (...args: any[]) => any>(
+  callback: T | undefined,
+  deps: DependencyList = [],
+) {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  return useCallback(((...args) => callbackRef.current?.(...args)) as T, deps);
 }
