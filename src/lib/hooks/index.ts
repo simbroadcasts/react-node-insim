e92c383b84387b7cb27b02e29b579118a69a5271
Martin Kapal <makap@ciklum.com>
2025-01-20 19:08:00 +0100

    Remove unused import

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -4,7 +4,6 @@ export {
   useConnections,
 } from './useConnections';
 export { useInSim } from './useInSim';
-export { useMessage } from './useMessage';
 export { useOnConnect } from './useOnConnect';
 export { useOnDisconnect } from './useOnDisconnect';
 export { useOnPacket } from './useOnPacket';
