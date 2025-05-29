131bc12644b1f0e6afe04629e707b59e80bbc156
Martin Kapal <flamecze@gmail.com>
2025-05-29 13:32:32 +0200

    Test InSim connection

---
+++ tests/connection.test.ts
@@ -0,0 +1,61 @@
+import Mitm from 'mitm';
+
+import { createRoot } from '../src';
+
+function stringToBytes(string: string) {
+  return string.split('').map((char) => char.charCodeAt(0));
+}
+
+describe('InSim connection', () => {
+  let mitm: ReturnType<typeof Mitm>;
+
+  beforeEach(() => {
+    mitm = Mitm();
+  });
+
+  afterEach(() => {
+    mitm.disable();
+  });
+
+  it('should connect to InSim and send an IS_ISI packet', (done) => {
+    // eslint-disable-next-line prefer-const
+    let root: ReturnType<typeof createRoot>;
+
+    mitm.on('connection', (socket, opts) => {
+      expect(opts.host).toEqual('127.0.0.1');
+      expect(opts.port).toEqual(29999);
+
+      socket.on('data', (data) => {
+        expect(data).toEqual(
+          Buffer.from([
+            11, // Size / 4
+            1, // PacketType.ISP_ISI
+            255, // ReqI = CONNECT_REQUEST_ID
+            0, // Zero
+            0, // UDPPort (1)
+            0, // UDPPort (2)
+            0, // InSimFlags.ISF_CON (1)
+            0, // InSimFlags.ISF_CON (2)
+            9, // InSimVer
+            '!'.charCodeAt(0), // Prefix
+            0, // Interval (1)
+            0, // Interval (2)
+            ...stringToBytes('adminPassword\0\0\0'), // Admin[16]
+            ...stringToBytes('Test App\0\0\0\0\0\0\0\0'), // IName[16]
+          ]),
+        );
+
+        root.disconnect();
+        done();
+      });
+    });
+
+    root = createRoot({
+      name: 'Test App',
+      host: '127.0.0.1',
+      port: 29999,
+      adminPassword: 'adminPassword',
+      prefix: '!',
+    });
+  });
+});
