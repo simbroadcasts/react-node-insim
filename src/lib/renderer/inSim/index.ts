80e197bdf481569dc5dbf2469b7422474f1e5e28
Martin Kapal <flamecze@gmail.com>
2025-06-03 20:10:35 +0200

    Remove static connect request ID

--- src/lib/renderer/inSim/index.ts
+++ src/lib/renderer/inSim/index.ts
@@ -1,7 +1,3 @@
-export {
-  type CreateRootOptions,
-  CONNECT_REQUEST_ID,
-  createRoot,
-} from './createRoot';
+export { type CreateRootOptions, createRoot } from './createRoot';
 export * from './elements';
 export * from './types';
