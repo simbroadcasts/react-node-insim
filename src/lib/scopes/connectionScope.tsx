79a35e533a5dfb87ed15ba6c05ec5eef52c4c003
Martin Kapal <flamecze@gmail.com>
2025-06-03 23:49:56 +0200

    Revert "Test use relative paths in connection scope"
    This reverts commit 3af60cb06c3de8212fc93e614458ea232f1464c4.

--- src/lib/scopes/connectionScope.tsx
+++ src/lib/scopes/connectionScope.tsx
@@ -2,12 +2,12 @@ import { createScope, molecule, use } from 'bunshi';
 import { ScopeProvider, useMolecule } from 'bunshi/react';
 import { atom, useAtomValue } from 'jotai';
 import { type ReactNode } from 'react';
-
 import {
   type Connection,
   ConnectionsPlayersProvider,
   useConnections,
-} from '../hooks';
+} from 'react-node-insim';
+
 import { useConnectionsPlayersMaybeContext } from '../hooks/ConnectionsPlayersProvider';
 
 export const ConnectionScope = createScope<Connection | null>(null);
