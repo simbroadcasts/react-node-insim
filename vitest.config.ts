c148ab5c3969b4d0051a97d5d20bb40afd08b059
Martin Kapal <flamecze@gmail.com>
2025-06-04 22:50:17 +0200

    Fix module alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'react-node-insim': path.resolve(__dirname, './src/index'),
+    },
+  },
   test: {
     coverage: {
       reporter: ['json-summary'],
